f42d73fdd602c014bb8970e331dad2cb92ecf934
Charles Kettell <charleskettell@hotmail.co.uk>
2019-07-18 16:57:04 +0100

    almost there

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,6 +32,17 @@ class ApplicationManager < Sinatra::Base
     redirect '/bookmarks'
   end
 
+  get '/bookmarks/:id/edit' do
+    @bookmark = Bookmark.find(id: params[:id])
+    erb (:update_bookmarks)
+    
+  end
+
+  patch '/bookmarks' do
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
+    redirect ('/bookmarks')
+  end
+
   run! if app_file == $0
 
 end
--- app/models/Bookmark.rb
+++ app/models/Bookmark.rb
@@ -31,5 +31,16 @@ class Bookmark
   def self.delete(id:)
     @connection.exec("DELETE FROM bookmarks WHERE id = #{id}")
   end
-end
 
+  def self.update(id:, url:, title:)
+    result = @connection.exec("UPDATE bookmarks SET url = '#{url}', title = '#{title}' WHERE id = #{id} RETURNING id, url, title;")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+  end
+
+  def self.find(id:)
+    result = @connection.exec("SELECT * FROM bookmarks WHERE id = #{id};")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+  end
+
+
+end
